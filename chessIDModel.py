d7340c3a7fb2270b2fc3a96bf2df065cc4412e3b
Swapnil Pande <swapnil.pande98@gmail.com>
2019-03-15 00:52:31 -0500

    Finished training script

--- chessIDModel.py
+++ chessIDModel.py
@@ -1,18 +1,19 @@
 from keras.applications.xception import Xception
 from keras.models import Model
-from keras.layers import Dense, GlobalAveragePooling2D, Dropout
+from keras.layers import Dense, GlobalAveragePooling2D, Dropout, Flatten
 
 # num_classes is the number of output classes
-def get_model(numClasses):
+def getModel(numClasses, inputDim):
     # Create base pre-trained model
-    baseModel = Xception(weights='imagent', include_top=False)
+    baseModel = Xception(weights='imagenet', include_top=False, input_shape = inputDim)
 
     # Get output of xception
-    xceptionOutput = baseModel.xceptionOutput
+    xceptionOutput = baseModel.output
     xceptionDropout = Dropout(0.5)(xceptionOutput)
+    xceptionFlat = Flatten()(xceptionDropout)
 
     # Layers appended to xception to train
-    chessIDDense = Dense(1024, activation='relu')(xceptionDropout)
+    chessIDDense = Dense(1024, activation='relu')(xceptionFlat)
     chessIDDropout = Dropout(0.5)(chessIDDense)
     chessIDOutput = Dense(numClasses, activation='softmax')(chessIDDropout)
 
--- train.py
+++ train.py
@@ -1,19 +1,56 @@
 import numpy as np
-import scipy.spatial as spatial
-import scipy.cluster as clstr
-from time import time
-from collections import defaultdict
-from functools import partial
-from sklearn.utils import shuffle
-import os, glob, skimage, cv2, shutil
 from matplotlib import pyplot as plt
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Model
+from keras.callbacks import ModelCheckpoint
 
-import dataUtils
-#import keras
+import chessIDModel
 
 # Image dimension
 SQUARE_SIDE_LENGTH = 227
 
+# Size of training dataset
+TRAIN_DATASET_SIZE = 10360
+
+# Relative directories to data
+trainDir = "./Chess ID Public Data/output_train/"
+testDir = "./processed_data/output_test/"
+
+
 # Categories for neural network to learn
 categories = ['bb', 'bk', 'bn', 'bp', 'bq', 'br', 'empty', 'wb', 'wk', 'wn', 'wp', 'wq', 'wr']
+
+# Create dataset generator
+batchSize = 32
+trainDataGenerator = ImageDataGenerator(rotation_range=270, horizontal_flip=True, rescale=1./255).flow_from_directory(
+    directory = trainDir,
+    target_size = (SQUARE_SIDE_LENGTH, SQUARE_SIDE_LENGTH),
+    classes = categories,
+    batch_size = batchSize,
+    shuffle = True,
+
+)
+
+# Define checkpoint callback
+shutil.rmtree("./checkpoints", True)
+os.mkdir("./checkpoints")
+callbacks = [ModelCheckpoint('./checkpoints/chess-id-checkpoint-{epoch:02d}-{train_loss:.2f}.hdf5')]
+
+# Instantiate model
+print(len(categories))
+model = chessIDModel.getModel(len(categories), (SQUARE_SIDE_LENGTH, SQUARE_SIDE_LENGTH, 3))
+model.compile("adam", loss= "categorical_crossentropy")
+model.summary()
+model.fit_generator(
+			trainDataGenerator,
+            steps_per_epoch = TRAIN_DATASET_SIZE // batchSize,
+			epochs = 50,
+			workers = 10,
+			shuffle=True,
+            callbacks = callbacks
+		)
+
+# Save final model
+model.save("./checkpoints/finalModel.hdf5")
+
 
