f3bed09412f94140f9060320c26a5455cc440005
Swapnil Pande <swapnil.pande98@gmail.com>
2019-03-16 16:44:01 -0500

    Moved many parameters to input arguments

--- chessIDModel.py
+++ chessIDModel.py
@@ -3,7 +3,7 @@ from keras.models import Model
 from keras.layers import Dense, GlobalAveragePooling2D, Dropout, Flatten
 
 # num_classes is the number of output classes
-def getModel(numClasses, inputDim):
+def getModel(numClasses, inputDim, trainAllLayers, numLayerToTrain):
     # Create base pre-trained model
     baseModel = Xception(weights='imagenet', include_top=False, input_shape = inputDim)
 
@@ -20,9 +20,18 @@ def getModel(numClasses, inputDim):
     # Create model object
     model = Model(inputs = baseModel.input, outputs = chessIDOutput)
 
+    print(len(baseModel))
+
     # Freeze all of the xception layers
-    for layer in baseModel.layers:
-        layer.trainable = False
+    if(not trainAllLayers):
+        # Disable training of all layers in xception model
+        for layer in baseModel.layers[]:
+            layer.trainable = False
+
+        #Enable last n layers to train
+        for layer in baseModel.layers[-1*numLayerToTrain:]:
+            layer.trainable = True
+    # else, we train all layers in the network, including xception layers
 
     return model
 
--- train.py
+++ train.py
@@ -1,12 +1,23 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from keras.preprocessing.image import ImageDataGenerator
-from keras.models import Model
+from keras.models import Model, load_model
 from keras.callbacks import ModelCheckpoint
 from keras.optimizers import Adam
 import shutil, os
+import argparse
+
 
 import chessIDModel
+
+#Retrieve command line options
+parser = argparse.ArgumentParser()
+parser.add_argument("-m", "--model", help="Path to model checkpoint to import weights from")
+parser.add_argument("-a", "--train-all-layers", help="Make all layers of the neural network trainable", action='store_true')
+parse.add_argument("-n", "--num-layer-to-train", help="Specify number of layers of xception model to train, starts from end", default = 0)
+parse.add_argument("-lr", "--learning-rate", help="Learning rate to use to for adam optimizer", default=0.0001)
+args =  parser.parse_args()
+
 
 # Image dimension
 SQUARE_SIDE_LENGTH = 227
@@ -40,8 +51,15 @@ callbacks = [ModelCheckpoint('./checkpoints/chess-id-checkpoint-{epoch:02d}-{los
 
 # Instantiate model
 #sgd = SGD(lr = 0.01, momentum = 0.9)
-adam = Adam(lr = 0.0001)
-model = chessIDModel.getModel(len(categories), (SQUARE_SIDE_LENGTH, SQUARE_SIDE_LENGTH, 3))
+adam = Adam(lr = args.lr)
+
+if(args.m is not None):
+    # Load new model - training of xception layers is disabled by default
+    model = chessIDModel.getModel(len(categories), (SQUARE_SIDE_LENGTH, SQUARE_SIDE_LENGTH, 3))
+else:
+    # Else we load the model
+    model = load_model(args.m)
+
 model.compile(adam, loss= "categorical_crossentropy", metrics = ["accuracy"])
 model.summary()
 model.fit_generator(
