e7625af80a705fd68424529f8a5df5a15329ba71
Swapnil Pande <swapnil.pande98@gmail.com>
2019-04-14 22:34:45 -0500

    Added programs to generate new data

--- generateNewData.py
+++ generateNewData.py
@@ -2,13 +2,13 @@ import os
 import cv2
 
 # Path to directory containing images of chessboards
-dataInputDir = ""
+dataInputDir = "./Board1/"
 
 # Path to file containing coordinates for chessboard squares
 coordinatePath = "square_locations.txt"
 
 # Directory to store segmented and labelled images
-dataOutputDir = ""
+dataOutputDir = "./output"
 
 # Ingest square coordinates from file
 squareCoords = []
@@ -20,29 +20,42 @@ with open(coordinatePath) as coordFile:
 
 boardImages = os.listdir(dataInputDir)
 
+print(boardImages)
 for image in boardImages:
+    print("Current images: " + image)
     # Get filename without extension
-    filename, _ = os.splittext(image)
+    filename, _ = os.path.splitext(image)
 
     # Read in image
-    frame = cv2.imread(filename)
+    frame = cv2.imread(os.path.join(dataInputDir, image))
 
     # Iterate over all square coordinates and segment image
     for i, squareCoord in enumerate(squareCoords):
         x1 = squareCoord[0]
         y1 = squareCoord[1]
         x2 = squareCoord[2]
-        x2 = squareCoord[3]
+        y2 = squareCoord[3]
+
+        print(squareCoord)
 
         # Slice image
         square = frame[y1:y2,x1:x2,:]
 
+        square = cv2.resize(square, (227,227))
+
         cv2.imshow(str(i), square)
-        label = raw_input("What chess piece is this?\n")
+        cv2.waitKey(1000)
+        label = input("What chess piece is this?\n")
+
 
         saveDir = os.path.join(dataOutputDir, label)
 
         # Save image in the correct directory
-        cv2.imwrite(filename + "-" + str(i), label)
+        outputPath = os.path.join(dataOutputDir, label, filename + "-" + str(i) + ".png")
+        cv2.imwrite(outputPath, square)
+
+
+        cv2.destroyAllWindows()
+
 
 
